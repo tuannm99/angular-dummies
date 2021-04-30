11df40b20ce75ca159f1ded0e1be5e7e556be1c2
minhtuan <gm.tuannm99@gmail.com>
2021-04-30 10:46:38 +0700

    single page app p2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -10,6 +10,7 @@ export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'menu', component: MenuComponent },
+  { path: 'dishdetail/:id', component: DishdetailComponent },
   { path: 'aboutus', component: AboutComponent },
   { path: 'contactus', component: ContactComponent },
 ];
