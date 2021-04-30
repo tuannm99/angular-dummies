a98211f4194375d080809cf8b287f1c4cd82463e
minhtuan <gm.tuannm99@gmail.com>
2021-04-30 23:55:32 +0700

    assignment 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -64,6 +65,7 @@ import { LoginComponent } from './login/login.component';
     MatCheckboxModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatSliderModule,
     FormsModule,
     ReactiveFormsModule,
   ],
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comment } from '../shared/comment';
 
 @Component({
   selector: 'app-dishdetail',
@@ -12,16 +14,37 @@ import { switchMap } from 'rxjs/operators';
   styleUrls: ['./dishdetail.component.scss'],
 })
 export class DishdetailComponent implements OnInit {
+  commentForm: FormGroup;
   dish: Dish;
+  comment: Comment;
   dishIds: string[];
   prev: string;
   next: string;
+  @ViewChild('cform') commentFormDirective;
+
+  formErrors = {
+    author: '',
+    comment: '',
+  };
+
+  validationMessages = {
+    author: {
+      required: 'Author is required.',
+      minlength: 'Author must be at least 2 characters long.',
+    },
+    comment: {
+      required: 'Comment is required.',
+    },
+  };
 
   constructor(
     private dishService: DishService,
     private location: Location,
-    private route: ActivatedRoute
-  ) {}
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
+  ) {
+    this.createForm();
+  }
 
   ngOnInit(): void {
     this.dishService
@@ -49,5 +72,57 @@ export class DishdetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  createForm() {
+    this.commentForm = this.formBuilder.group({
+      author: ['', [Validators.required, Validators.minLength(2)]],
+      rating: 5,
+      comment: ['', [Validators.required]],
+    });
+
+    this.commentForm.valueChanges.subscribe((data) =>
+      this.onValueChanged(data)
+    );
+
+    this.onValueChanged();
+  }
+
+  onSubmit() {
+    this.comment = {
+      rating: this.commentForm.value.rating,
+      author: this.commentForm.value.author,
+      comment: this.commentForm.value.comment,
+      date: new Date().toISOString()
+    }
+    this.dish.comments.push(this.comment)
+    this.commentFormDirective.resetForm();
+    this.commentForm.reset({
+      author: '',
+      rating: 5,
+      comment: '',
+    });
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.commentForm) {
+      return;
+    }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
 }
